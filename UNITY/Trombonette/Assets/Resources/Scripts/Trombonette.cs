ac7aac0c6cf6aac08c3187ac1f223e869e526f75
Steve557mag-fr <cbuchelotsteve557@gmail.com>
2025-02-10 22:17:52 +0100

    push scene

---
+++ UNITY/Trombonette/Assets/Resources/Scripts/Trombonette.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using System.IO.Ports;
+
+public class Trombonette : MonoBehaviour
+{
+    [Header("Conx Params")]
+    [SerializeField] string COMName = "COM A";
+    [SerializeField] int COMBitrate = 9600;
+    SerialPort serial;
+
+    public void OpenCOM()
+    {
+        serial = new SerialPort(COMName, COMBitrate);
+        serial.Open();
+    }
+
+    public void CloseCOM()
+    {
+        serial.Close();
+    }
+
+    void Start()
+    {
+        serial = new SerialPort();
+    }
+
+    void Update()
+    {
+        if (serial == null || !serial.IsOpen) return;
+
+
+
+    }
+}
