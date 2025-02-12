791e2d475c1784ffd9babd0ee60480b0d6421c18
COL1 C'EST MOI <57326899+Steve557mag-fr@users.noreply.github.com>
2025-02-12 07:06:38 +0100

    Squashed commit of the following:
    commit 3e5231f1364243e7a0954c62e2bf27e8878630cd
    Author: COL1 C'EST MOI <57326899+Steve557mag-fr@users.noreply.github.com>
    Date:   Wed Feb 12 07:05:56 2025 +0100
    
        Update sketch_trombenette.ino
    
    commit c96fd5a4e8640656c12f03af2fdc3b3aa38f681c
    Author: COL1 C'EST MOI <57326899+Steve557mag-fr@users.noreply.github.com>
    Date:   Tue Feb 11 09:10:08 2025 +0100
    
        Update SampleScene.unity
    
    commit 4ecdcd29e2c623c33d657616a2cf211b25db9d6f
    Author: COL1 C'EST MOI <57326899+Steve557mag-fr@users.noreply.github.com>
    Date:   Tue Feb 11 08:45:50 2025 +0100
    
        Add Serial unitypackages
    
    commit 37ce30495491e83529cc425b43c81ebb9011f232
    Author: Steve557mag-fr <cbuchelotsteve557@gmail.com>
    Date:   Tue Feb 11 00:17:29 2025 +0100
    
        Add more controls
    
    commit 49afac20d3bebdf87b2b6e7b926455f8056be345
    Author: Steve557mag-fr <cbuchelotsteve557@gmail.com>
    Date:   Mon Feb 10 23:55:09 2025 +0100
    
        Add ui debug
    
    commit d2e6473cef7f663fd5782e795e07e1a2f2150c3b
    Author: Steve557mag-fr <cbuchelotsteve557@gmail.com>
    Date:   Mon Feb 10 23:54:07 2025 +0100
    
        push nA
    
        - Add TMPro
        - Add JSON PARSING
        - Add COM start()
    
    commit 8558a39c1db4e991b7d1e96c3885b89e4c388c66
    Author: Steve557mag-fr <cbuchelotsteve557@gmail.com>
    Date:   Mon Feb 10 22:34:41 2025 +0100
    
        Rearange
    
    commit 20762b6c85be8fcdd305d96d1af64b26cf09fb20
    Author: Steve557mag-fr <cbuchelotsteve557@gmail.com>
    Date:   Mon Feb 10 22:33:13 2025 +0100
    
        Squashed commit of the following:
    
        commit d6c1291d2ab7b834bc67e355af700642209f24ee
        Author: Steve557mag-fr <cbuchelotsteve557@gmail.com>
        Date:   Mon Feb 10 22:25:40 2025 +0100
    
            Squashed commit of the following:
    
            commit ff4b7855b7850a89c9292663ed6c8d1e50bc3690
            Author: Steve557mag-fr <cbuchelotsteve557@gmail.com>
            Date:   Mon Feb 10 22:25:26 2025 +0100
    
                Update camera
    
            commit ac7aac0c6cf6aac08c3187ac1f223e869e526f75
            Author: Steve557mag-fr <cbuchelotsteve557@gmail.com>
            Date:   Mon Feb 10 22:17:52 2025 +0100
    
                push scene
    
            commit b4911f8a8b3e0fdb1f2144352486dfdc47278eab
            Author: COL1 C'EST MOI <57326899+Steve557mag-fr@users.noreply.github.com>
            Date:   Mon Feb 10 20:36:24 2025 +0100
    
                Add Serial unitypackage
    
        commit e42f1dfc371e31607742788eba489f72f91eb061
        Author: Steve557mag-fr <cbuchelotsteve557@gmail.com>
        Date:   Mon Feb 10 20:40:05 2025 +0100
    
            Squashed commit of the following:
    
            commit b4911f8a8b3e0fdb1f2144352486dfdc47278eab
            Author: COL1 C'EST MOI <57326899+Steve557mag-fr@users.noreply.github.com>
            Date:   Mon Feb 10 20:36:24 2025 +0100
    
                Add Serial unitypackage
    
        commit 7f5e8c56948c9d0c53b44a0eaa6b56e56fa3e5b5
        Author: COL1 C'EST MOI <57326899+Steve557mag-fr@users.noreply.github.com>
        Date:   Mon Feb 10 19:25:10 2025 +0100
    
            init part 2
    
    commit ff4b7855b7850a89c9292663ed6c8d1e50bc3690
    Author: Steve557mag-fr <cbuchelotsteve557@gmail.com>
    Date:   Mon Feb 10 22:25:26 2025 +0100
    
        Update camera
    
    commit ac7aac0c6cf6aac08c3187ac1f223e869e526f75
    Author: Steve557mag-fr <cbuchelotsteve557@gmail.com>
    Date:   Mon Feb 10 22:17:52 2025 +0100
    
        push scene
    
    commit b4911f8a8b3e0fdb1f2144352486dfdc47278eab
    Author: COL1 C'EST MOI <57326899+Steve557mag-fr@users.noreply.github.com>
    Date:   Mon Feb 10 20:36:24 2025 +0100
    
        Add Serial unitypackage

--- ARDUINO/sketch_trombenette/sketch_trombenette.ino
+++ ARDUINO/sketch_trombenette/sketch_trombenette.ino
@@ -1,12 +1,32 @@
+#include <Arduino_JSON.h>
 
-q
+#define A 41    // A : bouton A (digital  ; bool)
+#define B 39    // b : bouton B (digital  ; bool)
+#define C 37    // c : bouton C (digidal  ; bool)
+#define D 1     // d : coulisse (analog   ; int)
+#define M 0     // h : humidity sensor (analog)
+
 
 void setup() {
   // put your setup code here, to run once:
+  pinMode(A, INPUT);
+  pinMode(B, INPUT);
+  pinMode(C, INPUT);
   
+  Serial.begin(9600);
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  JSONVar data = JSONVar();
+  data["A"] = digitalRead(A);
+  data["B"] = digitalRead(B);
+  data["C"] = digitalRead(C);
+  data["D"] = analogRead(D);
+  data["H"] = analogRead(M);
+  Serial.println(data);
+
+  delay(1000/2);
 
 }
